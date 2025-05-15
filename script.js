94a858cfd909a3cc1b12070a1368b8686f09c865
Pitar254 <pitarminor@gmail.com>
2025-05-15 09:45:46 +0300

    Final Project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  function exploreShop() {
+    window.location.href = "shop.html";
+  }
+
+
+
+//   contact form script
+function submitForm(event) {
+    event.preventDefault();
+    document.getElementById("responseMessage").innerText = "Thank you! We will get back to you soon.";
+  }
